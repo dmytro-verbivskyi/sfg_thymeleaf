4c91c045768d59ce041786137b2f230bb9330194
Dmytro Verbivskyi <dverbovskiy@gmail.com>
2016-11-18 23:30:14 +0200

    init

---
+++ src/test/java/guru/spring/SfgThymeleafApplicationTests.java
@@ -0,0 +1,18 @@
+package guru.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SfgThymeleafApplication.class)
+@WebAppConfiguration
+public class SfgThymeleafApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
