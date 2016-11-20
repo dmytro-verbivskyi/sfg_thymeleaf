9fb0b99cf09f05bbfe29fa13461de909313b83b5
Dmytro Verbivskyi <dverbovskiy@gmail.com>
2016-11-20 22:42:16 +0200

    spring mvc

---
+++ src/main/java/guru/spring/controllers/ProductController.java
@@ -0,0 +1,13 @@
+package guru.spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProductController {
+
+    @RequestMapping("/product")
+    public String getProduct() {
+        return "product";
+    }
+}
